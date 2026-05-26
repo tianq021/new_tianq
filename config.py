2d1e550e5f1e53aa3f96c92086f43268a407e38d
乔浩天 <1506162029@qq.com>
2026-05-26 19:06:14 +0800

    添加.env

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-DB_HOST = "127.0.0.1"
-DB_PORT = 3306
-DB_USER = "root"
-DB_PASSWORD = "123456"
-DB_NAME = "new_tianq"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "new_tianq")
